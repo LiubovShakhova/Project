d8f045e1f9b6dce9f32ad575293ba5f5d7f3f493
Liubov Shakhova <shakhova_l90@mail.ru>
2020-07-22 16:08:01 +0200

    модульная структура

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import '@babel/polyfill';
+import 'es6-promise';
+import 'formdata-polyfill';
+import 'fetch-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+import 'nodelist-foreach-polyfill';
+import 'element-remove-polyfill';
