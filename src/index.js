5e235490372f62705c97e1e51e9ffb574281710d
Liubov Shakhova <shakhova_l90@mail.ru>
2020-07-22 23:59:45 +0200

    модальное окно (c классом popup-call)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 'use strict';
-
 import '@babel/polyfill';
+import 'nodelist-foreach-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
 import 'es6-promise';
 import 'formdata-polyfill';
 import 'fetch-polyfill';
-import elementClosest from 'element-closest';
-elementClosest(window);
-import 'nodelist-foreach-polyfill';
 import 'element-remove-polyfill';
+
+import togglePopUp from './modules/togglePopUp';
+
+
+//PopUp on link
+togglePopUp();
---
+++ src/modules/togglePopUp.js
@@ -0,0 +1,52 @@
+const togglePopUp = () => {
+  const callBtn = document.querySelectorAll('a.call-btn'),
+      popUpCall = document.querySelector('.popup-call'),
+			userName = document.querySelector('input#name_1'),
+			userPhone = document.querySelector('input#phone_1');
+
+  //анимация появления модального окна -----------------
+	let counter = 0;
+	const animate = () => {
+		const animation = requestAnimationFrame(animate);
+		if (counter >= 1) {
+			counter = 0;
+			return cancelAnimationFrame(animation);
+		}
+		counter += 0.05;
+		popUpCall.style.opacity = counter;
+  };
+	//------------------------------------
+	
+  callBtn.forEach(elem => {
+		elem.addEventListener('click', () => {
+			popUpCall.style.display = 'block';
+			if (!userName.hasAttribute('required') && !userPhone.hasAttribute('required')) {
+				userName.setAttribute('required', 'true');
+				userPhone.setAttribute('required', 'true');
+			}
+			if (document.body.clientWidth >= 576) {
+				animate();
+			}
+		});
+  });
+  
+  popUpCall.addEventListener('click', event => {
+		let target = event.target;
+
+		if (target.classList.contains('popup-close')) {
+			popUpCall.style.display = 'none';
+			if (userName.hasAttribute('required') && userPhone.hasAttribute('required')) {
+				userName.removeAttribute('required');
+				userPhone.removeAttribute('required');
+			} 
+		} else {
+			target = target.closest('.popup-content');
+
+			if (!target) {
+				popUpCall.style.display = 'none';
+			}
+		}
+	});
+};
+
+export default togglePopUp;
