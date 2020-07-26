6e0e721cffb70ba358a2caac8887a83620c42122
Liubov Shakhova <shakhova_l90@mail.ru>
2020-07-27 01:12:41 +0200

    Аккордеон

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -1,30 +1,37 @@
 const accordion = () => {
-  const accordionTwo = document.getElementById('accordion-two'),
-  headings = accordionTwo.querySelectorAll('.panel-heading'),
-      panels = accordionTwo.querySelectorAll('.panel-collapse');
+  const accordionOne = document.getElementById('accordion'),
+  accordionTwo = document.getElementById('accordion-two'),
+  headingsOne = accordionOne.querySelectorAll('.panel-heading'),
+  panelsOne = accordionOne.querySelectorAll('.panel-collapse'),
+  headingsTwo = accordionTwo.querySelectorAll('.panel-heading'),
+  panelsTwo = accordionTwo.querySelectorAll('.panel-collapse');
  
-  const toggleAccContent = (index) => {
-    for (let i = 0; i < panels.length; i++) {
-      if (index === i) {
-        panels[i].classList.add('in');
-      } else {
-        panels[i].classList.remove('in');
+  const toggleAllAcc = (accordion, headings, panels) => {
+    const toggleAccContent = (index) => {
+      for (let i = 0; i < panels.length; i++) {
+        if (index === i) {
+          panels[i].classList.add('in');
+        } else {
+          panels[i].classList.remove('in');
+        }
       }
-    }
+    };
+      
+    accordion.addEventListener('click', (event) => {
+      event.preventDefault();
+      let target = event.target;
+      target = target.closest('.panel-heading');
+        if (target) {
+          headings.forEach((item, i) => {
+            if (item === target) {
+              toggleAccContent(i);
+            }
+          });
+        }
+    });  
   };
-    
-  accordionTwo.addEventListener('click', (event) => {
-    event.preventDefault();
-    let target = event.target;
-    target = target.closest('.panel-heading');
-      if (target) {
-        headings .forEach((item, i) => {
-          if (item === target) {
-            toggleAccContent(i);
-          }
-        });
-      }
-  });  
+  toggleAllAcc(accordionOne, headingsOne, panelsOne);
+  toggleAllAcc(accordionTwo, headingsTwo, panelsTwo);
 };
 
 export default accordion;
--- src/modules/constructor.js
+++ src/modules/constructor.js
@@ -4,20 +4,6 @@ const constructor = () => {
       panels = accordionOne.querySelectorAll('.panel-collapse'),
       headings = accordionOne.querySelectorAll('.panel-heading');
 
-  //Change panels
-  for (let i = 0; i < headings.length; i++) {
-    headings[i].addEventListener('click', function(event) {
-      event.preventDefault();
-      this.classList.toggle('in');
-      let content = this.nextElementSibling;
-      if (content.classList.contains('in')) {
-        content.classList.remove('in');
-      } else {
-        content.classList.add('in');
-      }
-    });
-  }
-  // Next Step with nextBtn
   for (let i = 0; i < nextBtn.length; i++) {
     nextBtn[i].addEventListener('click', (e) => {
       e.preventDefault();
