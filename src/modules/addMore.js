953976a8e3217096b7d5ac1998362d2d834ef047
Liubov Shakhova <shakhova_l90@mail.ru>
2020-07-28 15:39:03 +0200

    Анимации

--- src/modules/addMore.js
+++ src/modules/addMore.js
@@ -2,7 +2,8 @@ const addMore = () => {
   const infoBlock = document.querySelector('.sentence'),
       addBtn = infoBlock.querySelector('.add-sentence-btn'),
       hiddenCards = infoBlock.querySelectorAll('.row > .hidden'),
-      visibleCard = infoBlock.querySelector('.visible-sm-block');
+      visibleCard = infoBlock.querySelector('.visible-sm-block'),
+      images = document.querySelectorAll('.img-box img');
 
   addBtn.addEventListener('click', () => {
     hiddenCards.forEach(elem => {
@@ -13,6 +14,29 @@ const addMore = () => {
       visibleCard.className = 'col-xs-12 col-sm-6 col-md-4';
     }
   });
+
+  //Animate Blocks
+  setTimeout(() => {
+    images[0].style.opacity = '1';
+  }, 300);
+  setTimeout(() => {
+    images[1].style.opacity = '1';
+  }, 700);
+  setTimeout(() => {
+    images[2].style.opacity = '1';
+  }, 1100);
+  setTimeout(() => {
+    images[3].style.opacity = '1';
+  }, 1400);
+  const h1 = document.querySelector('h1');
+  const h1Block = { count : 600, speed: 30, startPos: 600, endPos: 20 };
+  h1.style.marginTop = '600px';
+  const animateCalc = () => {
+    h1Block.count -= h1Block.speed;
+    h1.style.marginTop = `${h1Block.count}px`;
+    if (h1Block.count > h1Block.endPos) requestAnimationFrame(animateCalc);
+  };
+  requestAnimationFrame(animateCalc);
 };
 
 export default addMore;
